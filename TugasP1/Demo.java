d558075c84c9a98f817f359a82c6ef93da8860a5
Nerazuriancta <nerasyahputri@gmail.com>
2026-09-01 23:37:54 +0700

    Tugas Praktikum

--- TugasP1/Demo.java
+++ TugasP1/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
         // Membuat object setiap class
         Kipas kipas = new Kipas("Miyako", 1);
 
-        KipasElektrik kipasElektrik = new KipasElektrik("Miniso",2,2000,0.2);
+        KipasElektrik kipasElektrik = new KipasElektrik("Miniso",2,
+        2000,0.2);
 
         KipasMeja kipasMeja = new KipasMeja("Cosmos",1,30,3);
 
