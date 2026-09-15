94b7c1e07600dce7ebfda48737b87b80e556602b
Nerazuriancta <nerasyahputri@gmail.com>
2026-09-15 07:46:53 +0700

    Percobaan_1

---
+++ Pertemuan3/MotorEncapsulation/Motor.java
@@ -0,0 +1,40 @@
+package Pertemuan3.MotorEncapsulation;
+
+public class Motor {
+    private int kecepatan = 0;
+    public boolean kontakOn = false;
+
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void MatikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off! \n");
+        }
+    }
+
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan "+kecepatan+"\n");
+    }
+}
