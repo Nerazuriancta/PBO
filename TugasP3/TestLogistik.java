523eacde896a07d8a4648c223004983afe94340a
Nerazuriancta <nerasyahputri@gmail.com>
2026-09-15 18:44:40 +0700

    nomor_6

--- TugasP3/TestLogistik.java
+++ TugasP3/TestLogistik.java
@@ -1,26 +1,27 @@
 package TugasP3;
 
+import java.util.Scanner;
 public class TestLogistik {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Kontainer kontainerAlfa = new Kontainer("REQ-9988", "PT. Maju Bersama", 5000);
 
-        System.out.println("Nama pemilik: " +kontainerAlfa.getNamaPemilik());
-        System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
+        System.out.println("Nama Pemilik: "+kontainerAlfa.getNamaPemilik());
+        System.out.println("Kapasitas maksimal: "+kontainerAlfa.getKapasitasMaksimal()+" kg");
 
-        System.out.println("\nMemasukkan muatan baru seberat 6.000 kg...");
-        kontainerAlfa.tambahMuatan(6000);
-        System.out.println("Berat muatan saat ini: " +kontainerAlfa.getBeratMuatanSaatIni()+ " kg");
+        System.out.print("\nMasukkan berat muatan yang ingin dimasukkan: ");
+        double beratMasuk = sc.nextDouble();
+        kontainerAlfa.tambahMuatan(beratMasuk);
 
-        System.out.println("\nMemasukkan muatan baru sebrat 4.000 kg...");
-        kontainerAlfa.tambahMuatan(4000);
         System.out.println("Berat muatan saat ini: "+kontainerAlfa.getBeratMuatanSaatIni()+ " kg");
 
-        System.out.println("\nMembongkar muat/menurunkan barang seberat 500 kg...");
-        kontainerAlfa.turunkanMuatan(500);
+        System.out.print("\nMasukkan berat muatan yang ingin dibongkar: ");
+        double beratTurun = sc.nextDouble();
+        kontainerAlfa.turunkanMuatan(beratTurun);
+
         System.out.println("Berat muatan saat ini: "+kontainerAlfa.getBeratMuatanSaatIni()+ " kg");
 
-        System.out.println("\nMembongkar muat/menurunkan barang seberat 1.500 kg...");
-        kontainerAlfa.turunkanMuatan(1500);
-        System.out.println("Berat muatan saat ini: "+kontainerAlfa.getBeratMuatanSaatIni()+ " kg");
+        sc.close();
     }
 }
